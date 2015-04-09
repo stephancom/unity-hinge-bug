2f3e9ae9e874f7dcf75e1535272a6ef1a6eaddb7
stephan.com <stephan@stephan.com>
2015-04-08 21:29:49 -0400

    first commit\
    illustrates unity bug

---
+++ Assets/TriggerDoor.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerDoor : MonoBehaviour {
+
+	private HingeJoint hinge;
+
+	// Use this for initialization
+	void Start () {
+		hinge = GetComponentInChildren<HingeJoint> ();
+	}
+
+	void OnTriggerEnter (Collider other) {
+		JointSpring spring;
+
+		spring = hinge.spring;
+		spring.targetPosition = 90f;
+		hinge.spring = spring;
+	}
+
+	void OnTriggerExit (Collider other) {
+		JointSpring spring;
+
+		spring = hinge.spring;
+		spring.targetPosition = 0f;
+		hinge.spring = spring;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+### Hinge bug
+
+this unity project appears to isolate a bug in scripting hinge joints
+
+walk forward and the targetPosition of the spring changes in the project, but the door does not move.
+
+change that value yourself in the project and it does move.
+
+I'm new to Unity, so perhaps I'm doing this wrong, either show me how to do this right, or it's a bug.  I want to be able to walk forward and have the door swing open.
